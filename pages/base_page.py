c390e20c87815c644bf6fc2ff88ed0701b074469
KarinaIbr <karinaibragimova54@gmail.com>
2026-07-13 15:55:18 -0700

    Refactor: separate native and JavaScript click actions

--- pages/base_page.py
+++ pages/base_page.py
@@ -6,23 +6,28 @@ from config.settings import TIMEOUT
 class BasePage:
     def __init__(self, driver):
         self.driver = driver
+        self.wait = WebDriverWait(driver, TIMEOUT)
 
     def open(self, url):
         self.driver.get(url)
 
+    def scroll_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
+
     def wait_for_visible_element(self, locator):
-        wait = WebDriverWait(self.driver, TIMEOUT)
-        return wait.until(EC.visibility_of_element_located(locator))
+        return self.wait.until(EC.visibility_of_element_located(locator))
 
     def wait_for_url_contains(self, text):
-        wait = WebDriverWait(self.driver, TIMEOUT)
-        return wait.until(EC.url_contains(text))
+        return self.wait.until(EC.url_contains(text))
 
     def wait_for_clickable_element(self, locator):
-        wait = WebDriverWait(self.driver, TIMEOUT)
-        return wait.until(EC.element_to_be_clickable(locator))
+        return self.wait.until(EC.element_to_be_clickable(locator))
 
     def click_element(self, locator):
         element = self.wait_for_clickable_element(locator)
-        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element)
+        element.click()
+
+    def js_click(self, locator):
+        element = self.wait_for_clickable_element(locator)
+        self.scroll_to_element(element)
         self.driver.execute_script("arguments[0].click();", element)
